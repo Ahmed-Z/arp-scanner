884b3f7562090e7208b586e338e7a937a2491b8f
Ahmed-Z <53438617+Ahmed-Z@users.noreply.github.com>
2022-12-10 12:55:11 +0000

    Added comments

--- arp-scanner.py
+++ arp-scanner.py
@@ -1,32 +1,32 @@
-import scapy.all as scapy
-import argparse
+import scapy.all as scapy #import the scapy module
+import argparse #import the argparse module
 
+#define the function to scan the network
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    arp_request_broadcast = broadcast/arp_request
-    answered_list= scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
-    return answered_list
+    arp_request = scapy.ARP(pdst=ip) #create an ARP request packet
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff') #create a broadcast packet
+    arp_request_broadcast = broadcast/arp_request #combine the ARP request and broadcast packet
+    answered_list= scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0] #send the packet and store the response in a list
+    return answered_list #return the response
 
-
+#define the function to get the vendor of the given mac address
 def get_mac_vendor(mac):
-    mac = mac.upper().replace(':','')[0:6]
-    with open("mac-vendor.txt","r") as f:
-        for line in f :
-            if mac in line:
-                return line[7:]
-    return 'Unknown' 
+    mac = mac.upper().replace(':','')[0:6] #convert the mac address to upper case, remove the colons and get the first 6 characters
+    with open("mac-vendor.txt","r") as f: #open the 'mac-vendor.txt' file in read mode
+        for line in f : #iterate through the lines in the file
+            if mac in line: #if the mac address is in the line
+                return line[7:] #return the vendor name from the line
+    return 'Unknown' #if the mac address is not found in the file, return 'Unknown'
 
-
+#define the main function
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n','--network', type=str, required=True)
-    args = parser.parse_args()
-    hosts = scan(args.network)
-    for host in hosts:
-        mac_vendor = get_mac_vendor(host[1].src).strip()
-        print(host[0].pdst + 2*'\t' + host[1].src + 2*'\t' + mac_vendor)
-
+    parser = argparse.ArgumentParser() #create a new ArgumentParser object
+    parser.add_argument('-n','--network', type=str, required=True) #add a new argument 'network' which is required and is a string
+    args = parser.parse_args() #parse the command line arguments
+    hosts = scan(args.network) #scan the network and get the response
+    for host in hosts: #iterate through the hosts in the response
+        mac_vendor = get_mac_vendor(host[1].src).strip() #get the vendor of the host
+        print(host[0].pdst + 2*'\t' + host[1].src + 2*'\t' + mac_vendor) #print the ip address, mac address and vendor of the host
 
 if __name__ == "__main__":
-    main()
+    main() #call the main function
